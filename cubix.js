a3093c4207410d379b957296ef5e8f9c030f7c75
TNTUSER55 <22753264+TNTUSER55@users.noreply.github.com>
2019-12-05 18:22:06 -0500

    Update cubix.js

--- cubix.js
+++ cubix.js
@@ -241,6 +241,8 @@ var Cubix = {
 
 			else if (char === "?") ip.d = (ip.d + (stack[stack.length-1] < 0 ? 3 : stack[stack.length-1] > 0 ? 1 : 0)) % 4;
 			else if (char === "!") { if (stack.length && stack[stack.length-1]) state = "skip"; }
+			else if (char === "C") ip.d = (ip.d + (stack[stack.length-1] < 0 ? 1 : stack[stack.length-1] > 0 ? 3 : 0)) % 4;
+
 
 			else if (char === "+") stack.push((stack[stack.length-2]||0)+(stack[stack.length-1]||0));
 			else if (char === "-") stack.push((stack[stack.length-2]||0)-(stack[stack.length-1]||0));
